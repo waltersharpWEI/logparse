d997390454deb786b2b19ba071782b886882f30d
Ubuntu <ubuntu@ip-172-31-12-236.ap-northeast-1.compute.internal>
2019-06-24 10:36:35 +0000

    with Time Budget parser

--- logparserTargetAccuracy.py
+++ logparserTargetAccuracy.py
@@ -8,17 +8,10 @@ import sys
 with open(sys.argv[1],'rt') as f:
   tl = "bad"
   ct = 0
-  timeb = 0
   for line in f:
     line = line.strip('\n')
-    if re.match(r"##",line):
-      print(line)
-      timeb = int(line.split(" ")[-2])
-    if timeb == 0:
-      continue
     if re.match(r"Target accuracy",line):
       print("targetAccuracy=" + line.split(" ")[-1])
-      print("timeBudget=" + str(timeb))
     if re.match(r"Dataset",line):
       print(line)
     if re.match(r"^Time",line,re.IGNORECASE):
@@ -26,19 +19,16 @@ with open(sys.argv[1],'rt') as f:
       print(tt[-1])
     if re.match(r"^Sigma",line):
       tt = line.split(" ")
-      ct = 3
+      ct = 2
       print("sigma=" + tt[-1])
-    elif ct == 3:
+    elif ct == 2:
       if not re.match(r"^kimg",line):
         print("iter=" + line)
         ct -= 1
-    elif ct == 2:
+    elif ct == 1:
       if not re.match(r"^kimg",line):
         print("time=" + line)
         ct -= 1
-    elif ct == 1:
-      if not re.match(r"^kimg",line):
-        print("accuracy=" + line)
-        ct -= 1
+      
     tl = line
   
